2a0640b3518b3dca20ec85d8803f58c059f9698d
Anant Shukla <anantshukla404@gmail.com>
2024-07-30 10:44:06 +0530

    testing logging using the json data url

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const app = express() 
+const app = express();
+const axios = require('axios');
+const { copyFileSync } = require('fs');
 
 const port = 3000 || config.port
 
@@ -8,5 +10,26 @@ app.listen(port, (err) => {
     if(err) return console.log(`Error occured : ${err}`)
     console.log(`testing the server, running on port ${port}`)
 
+})
+
+let TempData = []; // non persistent form of memory
+
+const fetchData = async (url) => {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    TempData.push(data);
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+const url = 'https://microsoftedge.github.io/Demos/json-dummy-data/256KB.json';
+fetchData(url);
+
+app.get('/data', (req, res) => {
+    res.json(TempData);
+    console.log(TempData[0].length);
 })
 
